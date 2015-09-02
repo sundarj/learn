e544ccfbcf13196a29c5c28152040b7618b9df1f
SundarJ <sundarjoshi@hotmail.co.uk>
2015-09-02 01:01:53 +0100

    python hello world

---
+++ Python/hello.py
@@ -0,0 +1,15 @@
+
+# print "Hello, World!"
+
+from cStringIO import StringIO
+import sys
+
+_stdout = sys.stdout
+sys.stdout = stream = StringIO()
+
+import __hello__
+
+sys.stdout = _stdout
+
+replacements = ('...', '!'), (' ', ', ')
+print reduce(lambda s, kv: s.replace(*kv), replacements, stream.getvalue().title().strip())
